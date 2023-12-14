8f0e1ce33a08e235a44305d0303ff9da2c623443
bens8235 <bmshreeve3@gmail.com>
2023-12-14 15:32:24 +0000

    adding buttons and changing pictures

--- app.js
+++ app.js
@@ -10,40 +10,36 @@ function changeImage(i) {
   [thumbnails[i].src, thumbnails[i].alt] = [mainImageSrc, mainImageAlt];
 }
 
-function rightButtonEvent(event) {
-  rightButton.addEventListener(event, function () {
-    changeImage(thumbnails.length - 1);
-    lastImageSrc = thumbnails[thumbnails.length - 1].src;
-    lastImagealt = thumbnails[thumbnails.length - 1].alt;
-    for (let i = thumbnails.length - 1; i >= 0; i--) {
-      if (i === 0) {
-        [thumbnails[i].src, thumbnails[i].alt] = [lastImageSrc, lastImagealt];
-      } else {
-        [thumbnails[i].src, thumbnails[i].alt] = [
-          thumbnails[i - 1].src,
-          thumbnails[i - 1].alt,
-        ];
-      }
+function rightButtonEvent() {
+  changeImage(thumbnails.length - 1);
+  lastImageSrc = thumbnails[thumbnails.length - 1].src;
+  lastImagealt = thumbnails[thumbnails.length - 1].alt;
+  for (let i = thumbnails.length - 1; i >= 0; i--) {
+    if (i === 0) {
+      [thumbnails[i].src, thumbnails[i].alt] = [lastImageSrc, lastImagealt];
+    } else {
+      [thumbnails[i].src, thumbnails[i].alt] = [
+        thumbnails[i - 1].src,
+        thumbnails[i - 1].alt,
+      ];
     }
-  });
+  }
 }
 
-function leftButtonEvent(event) {
-  leftButton.addEventListener("click", function () {
-    changeImage(0);
-    firstImageSrc = thumbnails[0].src;
-    firstImagealt = thumbnails[0].alt;
-    for (let i = 0; i < thumbnails.length; i++) {
-      if (i === thumbnails.length - 1) {
-        [thumbnails[i].src, thumbnails[i].alt] = [firstImageSrc, firstImagealt];
-      } else {
-        [thumbnails[i].src, thumbnails[i].alt] = [
-          thumbnails[i + 1].src,
-          thumbnails[i + 1].alt,
-        ];
-      }
+function leftButtonEvent() {
+  changeImage(0);
+  firstImageSrc = thumbnails[0].src;
+  firstImagealt = thumbnails[0].alt;
+  for (let i = 0; i < thumbnails.length; i++) {
+    if (i === thumbnails.length - 1) {
+      [thumbnails[i].src, thumbnails[i].alt] = [firstImageSrc, firstImagealt];
+    } else {
+      [thumbnails[i].src, thumbnails[i].alt] = [
+        thumbnails[i + 1].src,
+        thumbnails[i + 1].alt,
+      ];
     }
-  });
+  }
 }
 
 for (let i = 0; i < thumbnails.length; i++) {
@@ -52,5 +48,18 @@ for (let i = 0; i < thumbnails.length; i++) {
   });
 }
 
-rightButtonEvent("click");
-leftButtonEvent("click");
+rightButton.addEventListener("click", function () {
+  rightButtonEvent();
+});
+leftButton.addEventListener("click", function () {
+  leftButtonEvent();
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.code === "ArrowLeft") {
+    leftButtonEvent();
+  }
+  if (event.code === "ArrowRight") {
+    rightButtonEvent();
+  }
+});
